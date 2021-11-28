f80eb139e2b154a8510a843df19f3949abfa6989
김지섭 <0226daniel@gmail.com>
2021-11-28 11:21:17 +0900

    fix: new notion html

--- main.js
+++ main.js
@@ -20,18 +20,19 @@ function createWindow() {
 
 	mainWindow.webContents.on('did-finish-load', function () {
 		mainWindow.webContents.insertCSS(
-			'::-webkit-scrollbar{ width: 10px;}' +
-				'::-webkit-scrollbar-track{ background: #f1f1f1; } ' +
-				'::-webkit-scrollbar-thumb{ background: #888; }' +
-				'::-webkit-scrollbar-thumb:hover{ background: #555; }' +
-				'div.notion-sidebar-container{display:none !important;}' + // 좌측 바
-				'div.notion-frame > div:nth-child(1){display:none !important;}' + // 상단 바
-				'div.notion-page-controls{display:none !important;}' + // 아이콘, 커버, 댓글 추가 버튼
-				'div.notion-help-button{display:none !important;}' + // 도움말 버튼
-				'div.notion-frame > div.notion-scroller{margin:40px 0 0 0;padding:20px 0 0 0;}' + // 스크롤
-				'div.notion-frame > div.notion-scroller > div:nth-child(1) > div{left: 0px !important;padding:0px !important;height:40px !important;-webkit-app-region: drag;position: fixed;top: 0px;background-color: gray;z-index: 100;}' + // 제목 셀
-				'div.notion-frame > div.notion-scroller.vertical.horizontal > div:nth-child(1) > div > div.notion-selectable.notion-page-block{height: 100%;}' + // 제목 셀 상단
-				'div.notion-frame > div.notion-scroller.vertical.horizontal > div:nth-child(1) > div > div.notion-selectable.notion-page-block > div{font-size:15px;vertical-align: middle;margin-left: 20px;}' + // 제목 텍스트
+			// '::-webkit-scrollbar{ width: 10px;}' +
+			// '::-webkit-scrollbar-track{ background: #f1f1f1; } ' +
+			// '::-webkit-scrollbar-thumb{ background: #888; }' +
+			// '::-webkit-scrollbar-thumb:hover{ background: #555; }' +
+			// 'div.notion-sidebar-container{display:none !important;}' + // 좌측 바
+			// 'div.notion-frame > div:nth-child(1){display:none !important;}' + // 상단 바
+			// 'div.notion-page-controls{display:none !important;}' + // 아이콘, 커버, 댓글 추가 버튼
+			'div.notion-help-button{display:none !important;}' + // 도움말 버튼
+				'#notion-app > div > div.notion-cursor-listener > div:nth-child(2) > div:nth-child(1) > div.notion-topbar > div:nth-child(1) > div:nth-child(3){-webkit-app-region: drag;height:100%;}' +
+				//'div.notion-frame > div.notion-scroller{margin:40px 0 0 0;padding:20px 0 0 0;}' + // 스크롤
+				// 'div.notion-frame > div.notion-scroller > div:nth-child(1) > div{left: 0px !important;padding:0px !important;height:40px !important;-webkit-app-region: drag;position: fixed;top: 0px;background-color: gray;z-index: 100;}' + // 제목 셀
+				// 'div.notion-frame > div.notion-scroller.vertical.horizontal > div:nth-child(1) > div > div.notion-selectable.notion-page-block{height: 100%;}' + // 제목 셀 상단
+				//'div.notion-frame > div.notion-scroller.vertical.horizontal > div:nth-child(1) > div > div.notion-selectable.notion-page-block > div{font-size:15px;vertical-align: middle;margin-left: 20px;}' + // 제목 텍스트
 				'div.notion-frame > div.notion-scroller > div:nth-child(2) > div{display:none !important;}' + // 제목 아래 여백
 				'div.notion-page-content{padding:0 20px 0 20px !important;}' + // 내용 페이지
 				'.notion-selectable{max-width: none !important;}' + // 각 셀
