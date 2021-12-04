c4fec04bef2d195386853908ad4069f5e9aebfcf
김지섭 <0226daniel@gmail.com>
2021-12-04 23:01:54 +0900

    feat: change many thing

--- main.js
+++ main.js
@@ -1,49 +1,27 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
 const { app, BrowserWindow } = require('electron');
 const cfg = require('electron-cfg');
 const console = require('console');
 const path = require('path');
+const fs = require('fs');
 app.console = new console.Console(process.stdout, process.stderr);
 
 function createWindow() {
 	const mainWindow = cfg.window().create({
 		minWidth: 200,
-		webPreferences: {
-			preload: path.join(__dirname, 'preload.js'),
-		},
 		frame: false,
 	});
 
-	mainWindow.loadURL(process.env.PAGE_URL);
+	mainWindow.loadURL('https://www.notion.so');
 
-	mainWindow.webContents.on('did-finish-load', function () {
-		mainWindow.webContents.insertCSS(
-			// '::-webkit-scrollbar{ width: 10px;}' +
-			// '::-webkit-scrollbar-track{ background: #f1f1f1; } ' +
-			// '::-webkit-scrollbar-thumb{ background: #888; }' +
-			// '::-webkit-scrollbar-thumb:hover{ background: #555; }' +
-			// 'div.notion-sidebar-container{display:none !important;}' + // 좌측 바
-			// 'div.notion-frame > div:nth-child(1){display:none !important;}' + // 상단 바
-			// 'div.notion-page-controls{display:none !important;}' + // 아이콘, 커버, 댓글 추가 버튼
-			'div.notion-help-button{display:none !important;}' + // 도움말 버튼
-				'.notion-topbar{-webkit-app-region: drag;height:100%;}' + // 드래그 영역
-				'.notion-topbar>*>*{-webkit-app-region: no-drag;}' + // 드래그 안되게
-				'@media (max-width: 600px){.notion-topbar-action-buttons{visibility: hidden;width:0}}' + // 창 작아지면 기능 버튼들 숨기기
-				//'div.notion-frame > div.notion-scroller{margin:40px 0 0 0;padding:20px 0 0 0;}' + // 스크롤
-				'div.notion-frame > div.notion-scroller > div:nth-child(1) > div{left: 0px !important;padding:0px !important;height:40px !important;-webkit-app-region: drag;position: fixed;top: 0px;background-color: gray;z-index: 100;}' + // 제목 셀
-				// 'div.notion-frame > div.notion-scroller.vertical.horizontal > div:nth-child(1) > div > div.notion-selectable.notion-page-block{height: 100%;}' + // 제목 셀 상단
-				//'div.notion-frame > div.notion-scroller.vertical.horizontal > div:nth-child(1) > div > div.notion-selectable.notion-page-block > div{font-size:15px;vertical-align: middle;margin-left: 20px;}' + // 제목 텍스트
-				'div.notion-frame > div.notion-scroller > div:nth-child(2) > div{display:none !important;}' + // 제목 아래 여백
-				'div.notion-page-content{padding:0 20px 0 20px !important;}' + // 내용 페이지
-				'.notion-selectable{max-width: none !important;}' + // 각 셀
-				//'.notion-sidebar-container { display: none;}' +
-				''
-		);
+	fs.readFile(__dirname + '/style.css', function (err, data) {
+		if (err) console.log(err);
+
+		mainWindow.webContents.on('did-finish-load', function () {
+			mainWindow.webContents.insertCSS(data.toString());
+		});
 	});
 
-	//mainWindow.webContents.openDevTools();
+	// mainWindow.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
